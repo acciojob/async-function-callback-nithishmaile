5161a5e9e11730711fb8ea48c389ca25c85405fe
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-10-26 16:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
+const button=document.getElementById("btn");
+const div=document.getElementById("output");
+
+const getTitle=async ()=>{
+	const options={
+		method:"GET",
+	}
+
+	const response= await fetch(url,options)
+	const data=await response.json()
+	div.textContent=`${data.title}`
+	
+}
+
+button.addEventListener("click",getTitle)
 //your JS code here. If required.
